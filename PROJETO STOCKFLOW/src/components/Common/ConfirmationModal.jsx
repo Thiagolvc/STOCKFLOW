a1c45de5a4b95a5e77b63dd96667902c1d72feda
Thiago Oliveira <160791226+Thiagolvc@users.noreply.github.com>
2025-06-03 23:23:55 -0300

    Delete PROJETO STOCKFLOW directory

--- PROJETO STOCKFLOW/src/components/Common/ConfirmationModal.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ConfirmationModal = ({ show, message, onClose }) => { if (!show) return null; return ( <div className="modal" style={{ display: "block" }}> <div className="modal-content"> <span onClick={onClose} style={{ float: "right", cursor: "pointer" }}>×</span> <p>{message}</p> <button onClick={onClose}>OK</button> </div> </div> ); };
-
-export default ConfirmationModal;
